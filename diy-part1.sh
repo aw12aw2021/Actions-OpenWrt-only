1c167ced22b5b3a30ecf3920b59d6355d8e47236
yuwentao114 <49578217+yuwentao114@users.noreply.github.com>
2022-09-14 02:08:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,4 +5,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加feeds源
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >> feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
